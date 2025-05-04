1a15fdf51a4fc19b60f50fcebba318331c86af18
Nhan Le <nhanle0829.work@gmail.com>
2025-05-03 18:33:44 -0700

    complete __init__ game

--- wordle.py
+++ wordle.py
@@ -3,20 +3,23 @@ import numpy as np
 
 class WordleGame:
     def __init__(self):
-        self.target_word = None
+        self.target_word = self.choose_target_word()
         pass
 
     @staticmethod
-    def choose_target_word():
+    def choose_target_word() -> str:
         with open("./wordle-answers-alphabetical.txt") as in_file:
             words = [word.strip() for word in in_file.readlines()]
         print(words[:10])
         return np.random.choice(words)
 
     def new_game(self, start_word:str=None):
-        pass
+        self.target_word = self.choose_target_word()
+        if start_word is not None:
+            pass
+
+
 
 
 if __name__ == "__main__":
     game = WordleGame()
-    
